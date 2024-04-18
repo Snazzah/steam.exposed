660c7c11984d7ea5e20066ba9ca44e646bf3b626
Snazzah <me@snazzah.com>
2024-04-18 14:39:46 -0500

    fix: log 500s

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,9 @@
 import type { HandleServerError } from '@sveltejs/kit';
 
-export const handleError: HandleServerError = async ({ status }) => {
+export const handleError: HandleServerError = async ({ error, status }) => {
   if (status === 404) return;
-  if (status === 500) return { message: 'Server error...' };
+  if (status === 500) {
+    console.error('Server error!', error);
+    return { message: 'Server error...' };
+  }
 };
