263fd9e88ef88e5ff8d0de811a14b6714686b1db
Snazzah <me@snazzah.com>
2024-04-18 14:42:27 -0500

    fix: fix rendering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,13 @@
 	import '@fontsource-variable/inter';
 	import '@fontsource-variable/source-code-pro';
 	import '../app.css';
-  import { PUBLIC_PLAUSIBLE_HOSTNAME } from '$env/static/public';
+  import { PUBLIC_PLAUSIBLE_HOSTNAME, PUBLIC_DOMAIN } from '$env/static/public';
 </script>
 
 <svelte:head>
 	{#if PUBLIC_PLAUSIBLE_HOSTNAME}
 		<script
-			data-domain={location.hostname}
+			data-domain={PUBLIC_DOMAIN}
 			src="https://{PUBLIC_PLAUSIBLE_HOSTNAME}/js/script.js"
 		/>
 	{/if}
